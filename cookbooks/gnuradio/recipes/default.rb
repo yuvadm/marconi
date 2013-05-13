6a16b697143133985e1741c229ba20a15be707ae
Yuval Adam <yuv.adm@gmail.com>
2013-05-14 01:17:21 +0300

    Compile gnuradio

--- cookbooks/gnuradio/recipes/default.rb
+++ cookbooks/gnuradio/recipes/default.rb
@@ -52,3 +52,45 @@ bash 'Install rtl-sdr' do
         ldconfig
     EOH
 end
+
+bash 'Compile GNU Radio' do
+    user 'vagrant'
+    group 'vagrant'
+    cwd node[:workingdir] + '/gnuradio'
+    code <<-EOH
+        export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+        mkdir build && cd build
+        make clean
+        cmake -DENABLE_BAD_BOOST=ON ../
+        make clean
+        make
+    EOH
+end
+
+bash 'Install GNU Radio' do
+    cwd node[:workingdir] + '/gnuradio'
+    code <<-EOH
+        cd build
+        make install
+        ldconfig
+    EOH
+end
+
+bash 'Compile gr-osmosdr' do
+    user 'vagrant'
+    group 'vagrant'
+    cwd node[:workingdir] + '/gr-osmosdr'
+    code <<-EOH
+        cmake .
+        make clean
+        make
+    EOH
+end
+
+bash 'Install gr-osmosdr' do
+    cwd node[:workingdir] + '/gr-osmosdr'
+    code <<-EOH
+        make install
+        ldconfig
+    EOH
+end

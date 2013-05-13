6c38ad5c99d9a95c3a0df6e38c57d1cc95b4ed60
Yuval Adam <yuv.adm@gmail.com>
2013-05-13 14:10:18 +0300

    Check out git repos and refactor attrs

---
+++ cookbooks/gnuradio/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:workingdir] = "/home/vagrant/dev"
+
+default[:repos][:gnuradio] = "http://gnuradio.org/git/gnuradio.git"
+default[:repos][:rtlsdr] = "git://git.osmocom.org/rtl-sdr"
+default[:repos][:gr_osmosdr] = "git://git.osmocom.org/gr-osmosdr"
--- cookbooks/gnuradio/recipes/default.rb
+++ cookbooks/gnuradio/recipes/default.rb
@@ -1,51 +1,36 @@
-execute 'update package index' do
-  command 'apt-get update'
-  ignore_failure true
-  action :nothing
-end.run_action(:run)
+include_recipe 'gnuradio::packages'
 
-package 'libfontconfig1-dev'
-package 'libxrender-dev'
-package 'libpulse-dev'
-package 'swig'
-package 'g++'
-package 'automake'
-package 'autoconf'
-package 'libtool'
-package 'python-dev'
-package 'libfftw3-dev'
-package 'libcppunit-dev'
-package 'libboost1.48-all-dev'
-package 'libusb-dev'
-package 'libusb-1.0-0-dev'
-package 'fort77'
-package 'libsdl1.2-dev'
-package 'python-wxgtk2.8'
-package 'git-core'
-package 'guile-1.8-dev'
-package 'libqt4-dev'
-package 'python-numpy'
-package 'ccache'
-package 'python-opengl'
-package 'libgsl0-dev'
-package 'python-cheetah'
-package 'python-lxml'
-package 'doxygen'
-package 'qt4-dev-tools'
-package 'libusb-1.0-0-dev'
-package 'libqwt5-qt4-dev'
-package 'libqwtplot3d-qt4-dev'
-package 'pyqt4-dev-tools'
-package 'python-qwt5-qt4'
-package 'cmake'
-package 'git-core'
-package 'wget'
-package 'libxi-dev'
-package 'python-docutils'
-package 'gtk2-engines-pixbuf'
-package 'r-base-dev'
-package 'python-tk'
-package 'liborc-0.4-0'
-package 'liborc-0.4-dev'
-package 'libasound2-dev'
-package 'python-gtk2'
+directory node[:workingdir] do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0755'
+  action :create
+end
+
+git 'Checkout gnuradio' do
+    repository node[:repos][:gnuradio]
+    reference 'master'
+    action :checkout
+    destination node[:workingdir] + '/gnuradio'
+    user 'vagrant'
+    group 'vagrant'
+end
+
+git 'Checkout rtl-sdr' do
+    repository node[:repos][:rtlsdr]
+    reference 'master'
+    action :checkout
+    destination node[:workingdir] + '/rtl-sdr'
+    user 'vagrant'
+    group 'vagrant'
+end
+
+git 'Checkout gr-osmosdr' do
+    repository node[:repos][:gr_osmosdr]
+    reference 'master'
+    action :checkout
+    destination node[:workingdir] + '/gr-osmosdr'
+    user 'vagrant'
+    group 'vagrant'
+end
+
